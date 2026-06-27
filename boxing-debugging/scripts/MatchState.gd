8668cc2a5418b28d27b9eba309fec750da1e5e10
mnicol93 <marc.nicolasoliva@hotmail.com>
2026-06-27 22:26:11 +0200

    fix: multiply by RING_SCALE instead of adding in to_screen()

--- boxing-debugging/scripts/MatchState.gd
+++ boxing-debugging/scripts/MatchState.gd
@@ -4,3 +4,13 @@ extends Node
 
 var match_id: String = ""
 var ws_url: String = ""
+
+const RING_SCALE := 600.0 / 860.0
+const RING_OFFSET_X := (1280.0 - 600.0) / 2.0
+const RING_OFFSET_Y := (720.0 - 600.0) / 2.0
+
+func to_screen(backend_x: float, backend_y: float) -> Vector2:
+	return Vector2(
+		RING_OFFSET_X + backend_x * RING_SCALE,
+		RING_OFFSET_Y + backend_y * RING_SCALE
+	)
