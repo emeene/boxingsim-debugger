a525f095797ba82fc4a7a1f43ef41ffadfdc02e4
mnicol93 <marc.nicolasoliva@hotmail.com>
2026-06-28 18:05:54 +0200

    Smooth fighter position updates with lerp interpolation

--- boxing-debugging/scenes/Fighter.gd
+++ boxing-debugging/scenes/Fighter.gd
@@ -3,9 +3,14 @@ extends Node2D
 const RADIUS := 12.0
 @export var color: Color = Color.WHITE_SMOKE
 
+var _target_position: Vector2 = Vector2.ZERO
+
 func _draw() -> void:
 	draw_circle(Vector2.ZERO, RADIUS, color)
-	
-func update_from_snapshot(x: float, y: float) -> void:
-	position = MatchState.to_screen(x,y)
+
+func _process(delta: float) -> void:
+	position = position.lerp(_target_position, delta / 0.1)
 	queue_redraw()
+
+func update_from_snapshot(x: float, y: float) -> void:
+	_target_position = MatchState.to_screen(x, y)
